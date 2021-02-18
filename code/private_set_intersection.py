c366ffca28f1b38fe28e8d8233664b704286afdc
willclarktech <willclarktech@users.noreply.github.com>
2021-02-18 18:45:57 +0000

    Rename solution file

--- code/private_set_intersection.py
+++ code/private_set_intersection.py
@@ -7,52 +7,40 @@ DEFAULT_BIT_LENGTH = 2048  # For the secret exponents
 
 
 def hash_number(n: int) -> int:
-    n_bytes = n.to_bytes(ceil(n.bit_length() / 8), byteorder="big", signed=False)
-    hashed_bytes = sha256(n_bytes).digest()
-    return int.from_bytes(hashed_bytes, byteorder="big", signed=False)
+    pass
 
 
 class PSIAgent:
+    """
+    Base class for PSI agents.
+    """
+
     def __init__(
         self, initial_set: List[int], p: int, bit_length: int = DEFAULT_BIT_LENGTH
     ) -> None:
-        self.set = initial_set
-        self.p = p
-        key_space = 2 ** bit_length
-        self.secret = secrets.randbelow(key_space)
-
-    def is_generator(self, g: int) -> bool:
-        p = self.p
-        l = len(set([pow(g, i, p) for i in range(1, p)]))
-        return l == p - 1
-
-    def exponentiate(self, g: int) -> int:
-        return pow(g, self.secret, self.p)
-
-    def prepare_intermediate_key(self, n: int) -> int:
-        hashed_n = hash_number(n)
-        return (
-            self.exponentiate(hashed_n)
-            if self.is_generator(hashed_n)
-            else self.prepare_intermediate_key(hashed_n)
-        )
+        pass
 
     def prepare_intermediate_keys(self) -> List[int]:
-        return [self.prepare_intermediate_key(n) for n in self.set]
+        pass
 
 
 class PSIServer(PSIAgent):
+    """
+    PSI server class. Handles requests from clients.
+    """
+
     def handle_request(
         self, client_intermediate_keys: List[int]
     ) -> Tuple[List[int], List[int]]:  # (client_keys, server_intermediate_keys)
-        client_keys = [self.exponentiate(n) for n in client_intermediate_keys]
-        server_intermediate_keys = self.prepare_intermediate_keys()
-        return client_keys, server_intermediate_keys
+        pass
 
 
 class PSIClient(PSIAgent):
+    """
+    PSI client class. Handles responses from servers.
+    """
+
     def handle_response(
         self, client_keys: List[int], server_intermediate_keys: List[int]
     ) -> List[int]:
-        server_keys = [self.exponentiate(n) for n in server_intermediate_keys]
-        return [n for key, n in zip(client_keys, self.set) if key in server_keys]
+        pass
